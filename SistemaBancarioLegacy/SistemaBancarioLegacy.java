0418b13cc2047a5088a6c525ead5a6cd127862d9
andersonsua0331-rgb <andersonsua0331@gmail.com>
2026-02-19 01:30:45 +0000

    codigo final de sistema bancario

--- SistemaBancarioLegacy/SistemaBancarioLegacy.java
+++ SistemaBancarioLegacy/SistemaBancarioLegacy.java
@@ -1,18 +1,47 @@
 package SistemaBancarioLegacy;
 
+import java.util.ArrayList;
 import java.util.Vector;
 
 public class SistemaBancarioLegacy {
     public static void main(String[] args) {
         Vector<String> logTransacciones = new Vector<>();
 
-        logTransacciones.add("transaccion #1");
-        logTransacciones.add("transaccion #2");
-        logTransacciones.add("transaccion #3");
-        logTransacciones.add("transaccion #4");
-        logTransacciones.add("transaccion #5");
+        logTransacciones.add("transaccion 1");
+        logTransacciones.add("transaccion 2");
+        logTransacciones.add("transaccion 3");
+        logTransacciones.add("transaccion 4");
+        logTransacciones.add("transaccion 5");
+
+        System.out.println(logTransacciones);
 
   //se bloquean los objetos para no cruzar hilos 
+
+  int cantidad =1000000;
+
+  Vector<Double> vector= new Vector<>();
+  long inicioVector= System.currentTimeMillis();
+  for (int i=0; i<1000000;i++){
+    
+  }
+
+
+  ArrayList<Double> arrayList= new ArrayList<>();
+  long inicioArray= System.currentTimeMillis();
+  for (int i=0; i<1000000; i++){
+
+  }
+
+  long fin=System.currentTimeMillis();
+
+  System.out.println("Tiempo de vector: "+(fin-inicioVector));
+  System.out.println("Tiempo de ArrayList: "+(fin-inicioArray));
+
+
+
+
+
+  
 
     }
 }
